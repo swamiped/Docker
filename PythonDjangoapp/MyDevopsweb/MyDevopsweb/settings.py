090bb66e076ebdfa02754b02c54364c1a5dff4c0
swamiped <swamipedireddi99@gmail.com>
2025-02-28 21:14:14 +0530

    updated files

--- PythonDjangoapp/MyDevopsweb/MyDevopsweb/settings.py
+++ PythonDjangoapp/MyDevopsweb/MyDevopsweb/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'MyDevopsweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'mywebapp/templates')],
+        'DIRS': [os.path.join(BASE_DIR, '/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
